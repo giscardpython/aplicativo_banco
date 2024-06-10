570f29acc389d191b1abcb9a71bc08cd55633f00
Giscard Stephanou <giscard.silva@aluno.senai.br>
2024-06-10 19:54:47 -0300

    Versão .gitignore

--- aplicativo_banco.py
+++ aplicativo_banco.py
@@ -1,5 +1,6 @@
 import os
 from modulo import *
+from datetime import date
 
 indice = 0
 opcao = 0
@@ -10,17 +11,23 @@ while True:
     print ("\nAplicativo de Banco: \n")
     print('1 - Cadastrar nome no aplicativo')
     print('2 - Deletar nome no aplicativo')
-    print('3 - Entrar no aplicativo')
-    print('4 - Sair do programa')
+    print('3 - Entrar no aplicativo - Consultar o Saldo')
+    print('4 - Entrar no aplicativo - Deposita')
+    print('5 - Entrar no aplicativo - Saca')
+    print('6 - Sair do programa')
 
-    opcao1 = int(input('\nEscolha a opção desejada (1 a 4):\n'))
+    opcao1 = int(input('\nEscolha a opção desejada (1 a 6):\n'))
     
     match opcao1:
         case 1:
             while True:
                 novo_nome = input('Informe o novo nome a ser inserido no banco: ')
                 if novo_nome !='':
-                    numero_conta = indice + 1
+                    tamanho_dicionario = len(pessoas);
+                    if tamanho_dicionario == 0:
+                        numero_conta = 1
+                    else:
+                        numero_conta = tamanho_dicionario + 1    
                     saldo = 0
                     pessoa = {"Nome": novo_nome, "Numero da Conta": numero_conta, "Saldo": saldo}
                     pessoas.append(pessoa)
@@ -28,6 +35,7 @@ while True:
                     print(f'Número da Conta: {numero_conta}.')
                     print(f'Titular da Conta: {novo_nome}.')
                     print(f'Saldo: {saldo}.')
+                    extrato(novo_nome, saldo)
                     print('\nLista de Clientes:')
                     for nome in pessoas:
                         print(nome)        
@@ -38,25 +46,68 @@ while True:
                         print(nome)                
                     break
         case 2:
-            nome_a_excluir = input('Informe o nome que deseja retirar do banco: ')
+            indice_excluir = int(input('Informe o índice que deseja deletar da lista de dicionário: '))
+            indice_excluir -=1
             try:
-                pessoas.remove(nome_a_excluir)
+                del(pessoas[indice_excluir])
             except:
-                print('Nome não pode ser excluído.')
-
-            for nome in pessoas:
-                print(nome)        
+                print('Não foi possível deletar o índice!')
+            for i in range(len(pessoas)):
+                    print(f'Índice: {i + 1}')
+                    print(f'Nome: {pessoas[i]['Nome']}')
+                    print(f'Conta: {pessoas[i]['Numero da Conta']}')
+                    print(f'Saldo: {pessoas[i]['Saldo']}\n')
         case 3:
-            nome = input('Informe o nome que deseja encontrar: ').capitalize()
+            nome_busca = input('Informe o nome da pessoa que deseja consultar o saldo: ').capitalize()
             # busca o nome desejado
-            try:
-                indice = pessoas.index(nome)
-                quantidade = pessoas.count(nome)
-                print(f'Nome encontrado: {nome} {quantidade} vez(es) no índice {indice}')
-            except:
-                print(f'{nome} não encontrado no aplicativo.')
+            for i in range(len(pessoas)):
+                if nome_busca in pessoas[i]['Nome']:
+                    extrato(pessoas[i]['Nome'],pessoas[i]['Saldo'])
+                else:
+                    continue
         case 4:
+            nome_busca1 = input('Informe o nome da pessoa que deseja depositar: ').capitalize()
+            # busca o nome desejado
+            for i in range(len(pessoas)):
+                if nome_busca1 in pessoas[i]['Nome']:
+                    print(f'Índice: {i + 1}')
+                    print(f'Nome: {pessoas[i]['Nome']}')
+                    print(f'Conta: {pessoas[i]['Numero da Conta']}')
+                    print(f'Saldo: {pessoas[i]['Saldo']}')
+                    saldo_atual = pessoas[i]['Saldo']
+                    valor_deposito = int(input('Informe o valor que será depositado: '))
+                    if valor_deposito > 0:
+                        pessoas[i]['Saldo'] += valor_deposito
+                        print(f'Novo Saldo: {pessoas[i]['Saldo']}')
+                    else:    
+                        print('Digite um valor > 0')            
+                else:
+                    continue
+        case 5:
+            nome_busca2 = input('Informe o nome da pessoa que deseja sacar: ').capitalize()
+            # busca o nome desejado
+            for i in range(len(pessoas)):
+                if nome_busca2 in pessoas[i]['Nome']:
+                    print(f'Índice: {i + 1}')
+                    print(f'Nome: {pessoas[i]['Nome']}')
+                    print(f'Conta: {pessoas[i]['Numero da Conta']}')
+                    print(f'Saldo: {pessoas[i]['Saldo']}')
+                    saldo_atual = pessoas[i]['Saldo']
+                    valor_saque = int(input('Informe o valor que será sacado: '))
+                    if valor_saque > 0:
+                        pode_sacar(valor_saque, saldo_atual)
+                        saca(valor_saque, saldo_atual)
+                        if saldo_atual >= valor_saque:
+                            pessoas[i]['Saldo'] -= valor_saque
+                            print(f'Novo Saldo: {pessoas[i]['Saldo']}')
+                        else:
+                            print('Saldo insuficiente!')    
+                    else:    
+                        print('Digite um valor > 0')            
+                else:
+                    continue
+        case 6:
             break
         case _:
-            print('Opção inváida! Digite uma opção de 1 a 4')
+            print('Opção inváida! Digite uma opção de 1 a 6')
             continue      
